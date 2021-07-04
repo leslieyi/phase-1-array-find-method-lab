d1890e64c251e71dd1bb7c5a7b385792ea3d9e90
github username <github email address>
2021-07-04 11:24:39 -0400

    commit tree 629ac002304e9dddff51e4df2cab757c3373757b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function superbowlWin(record) {
+    // const found = record.find(( {result} ) => result === 'W');
+    // const found = record.find( => === "W")
+    const found = record.find( bob=> bob.result === "W");
+    if (found) {
+        return found.year
+    }
+}
 
+
+
+// Write a function called superbowlWin() in index.js:
+
+// The function should receive 1 argument, an Array of JavaScript Objects
+// Each object has two properties: year and result
+// It should use find() to test each Object to see if the result is "W" — a win!
+// It should return the year when the win occurred (if it occurred at all!)
+// If no win is found, it should return, sadly, undefined
+
+//if line 3 hits false the line 34 never runs. so the function is undefined.
